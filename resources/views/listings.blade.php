4559b14ebdf23fe6d29a38fc4a2e8dedf401feba
AJ Javadi <amirjavadi25@gmail.com>
2024-11-23 15:44:48 -0600

    added partials and finished updating theme

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,6 +1,10 @@
 @extends('layout')
 
 @section('content')
+{{-- HERO --}}
+@include('partials._hero')
+@include('partials._search')
+
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
 
@@ -13,7 +17,7 @@
                         <img class="hidden w-48 mr-6 md:block" src="{{asset('images/no-image.png')}}" alt="placeholder image" />
                         <div>
                             <h3 class="text-2xl">
-                                <a href="show.html">{{ $listing->title }}</a>
+                                <a href="/listings/{{$listing->id}}">{{ $listing->title }}</a>
                             </h3>
                             <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
                             <ul class="flex">
