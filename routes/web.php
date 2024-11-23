f54d5b0e3c44ece6cb6bf7968e525e7ffeb202d0
AJ Javadi <amirjavadi25@gmail.com>
2024-11-23 17:08:18 -0600

    refactored listings to get view from controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
@@ -9,20 +10,8 @@ use Illuminate\Support\Facades\Route;
  */
 
 //  All listings
-Route::get('/', function () {
-    return view('listings', [
-        'listings' => Listing::all()
-
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 
 //  Single Listings
-Route::get('/listings/{listing}', function (Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-
-
-
-});
+Route::get('/listings/{listing}',[ListingController::class, 'show']);
