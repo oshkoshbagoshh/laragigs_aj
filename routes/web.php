0bccfb35a8b72f64ea26ef09173ff3d69dc91439
AJ Javadi <amirjavadi25@gmail.com>
2024-11-23 15:53:54 -0600

    updated listing to use Eloquent find

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,11 @@ Route::get('/', function () {
 
 
 //  Single Listings
-Route::get('/listings/{id}', function($id) {
+Route::get('/listings/{listing}', function (Listing $listing) {
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
+
+
+
 });
