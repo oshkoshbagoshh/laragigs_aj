6d6b6f846e794a5b6b30a1df48eceb1debabaaa9
AJ Javadi <amirjavadi25@gmail.com>
2024-11-23 19:13:54 -0600

    added flash message with alpine js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,6 +11,8 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
         {{-- TODO: add meta tags --}}
     <script src="https://cdn.tailwindcss.com"></script>
+    {{-- ALPINE JS --}}
+    <script src="//unpkg.com/alpinejs" defer></script>
     <script>
         tailwind.config = {
             theme: {
@@ -60,6 +62,11 @@
 
         <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
     </footer>
+
+
+    {{-- Flash Message --}}
+    <x-flash-message />
+
 </body>
 
 </html>
